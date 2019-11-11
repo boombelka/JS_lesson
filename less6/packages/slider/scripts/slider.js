87d82a0bbae74aac3c406a3ad95492ac24a4723f
boombelka <av_cons@inbox.ru>
2019-11-12 00:27:29 +0300

    Урок 6 Создание слайда по аналогии с уроком -переписал скрипт со стрелками Не могу понять почему не загружается дальше страница. window.addEventListener('load', function () {     leftArrow.addEventListener('click', function () {         images.setNextLeftImage();

--- less6/packages/slider/scripts/slider.js
+++ less6/packages/slider/scripts/slider.js
@@ -3,9 +3,14 @@ document.head.insertAdjacentHTML("afterbegin", '<link rel="stylesheet" href="htt
 let slider = document.querySelector('.slider');
 
 //иконка загрузки
-let loadIcon = document.createElement('fas', 'fa-sprinner', 'fa-spin');
-loadIcon.classList.add("afterbegin", loadIcon);
+let loadIcon = document.createElement('i');
+loadIcon.classList.add('fas', 'fa-spinner', 'fa-spin');
 slider.insertAdjacentElement("afterbegin", loadIcon);
+// Создаем иконку загрузки
+//let loadIcon = document.createElement('i');
+//loadIcon.classList.add('fas', 'fa-spinner', 'fa-spin');
+//slider.insertAdjacentElement("afterbegin", loadIcon);
+
 
 //левая стрелка
 let leftArrow = document.createElement('i');
@@ -14,5 +19,81 @@ slider.insertAdjacentElement("beforeend", leftArrow);
 
 //правая стрелка
 let rightArrow = document.createElement('i');
-rightArrow.classList.add('fas', 'fa-chevron-circle-left', 'slider-leftArrow');
+rightArrow.classList.add('fas', 'fa-chevron-circle-right', 'slider-rightArrow');
 slider.insertAdjacentElement("beforeend", rightArrow);
+
+//ожидание загрузки страницы
+//window.addEventListener('load', function() {
+//    leftArrow.addEventListener('click', function () {
+//       images.setNextLeftImage();
+//    });
+window.addEventListener('load', function () {
+    leftArrow.addEventListener('click', function () {
+        images.setNextLeftImage();
+    });
+    rightArrow.addEventListener('click', function () {
+       images.setNextRightImage();
+    });
+    //инициализация слайдера
+    images.init();
+    //скрытие иконки
+    hideLoadIcon(loadIcon);
+    });
+
+//функция для скрытия иконки загрузки
+function hideLoadIcon(loadIcon) {
+    loadIcon.style.display = "none";
+}
+
+let images = {
+    currentIdx: 0,
+
+    slides: [],
+
+    init() {
+        this.slides = document.querySelectorAll('slider-component');
+        this.showImageWithCurrentIdx();
+
+    },
+    //открытие текущего слайда
+    //showImageWithCurrentIdx() {
+    //this.slides[this.currentIdx].classList.remove('hidden-slide')
+      //  const currentSlide = this.slides[this.currentIdx];
+        //currentSlide.classList.remove('hidden-slide');
+        //currentSlide.classList.remove('hidden-slide');
+        showImageWithCurrentIdx() {
+        const currentSlide = this.slides[this.currentIdx];
+        currentSlide.classList.remove('hidden-slide');
+    },
+
+//},
+    //перебор слайдов для выставления индекска hidden-slide
+    hideVisbleImages() {
+        this.slides.forEach(function (slide) {
+            //slide.classList.contains('hidden-slide')
+            slide.classList.add('hidden-slide');
+        });
+    },
+
+
+  //переключение на изображение влево hideVisbleImages или вправо hideVisbleImages
+    setNextLeftImage() {
+        this.hideVisbleImages();
+        if (this.currentIdx == 0) {
+            this.currentIdx = this.slides.length - 1;
+            } else {
+            this.currentIdx--;
+        }
+        this.showImageWithCurrentIdx();
+    },
+    setNextRightImage() {
+        this.hideVisbleImages();
+        if (this.currentIdx == 0) {
+            this.currentIdx = this.slides.length - 1;
+            } else {
+            this.currentIdx--;
+        }
+        this.showImageWithCurrentIdx();
+    },
+
+};
